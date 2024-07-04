c9437f56f2e08bced07f9c9314c902445aef065a
YangSungHun <ysh0208@strato.co.kr>
2024-07-04 16:41:36 +0900

    WIKI.md Update / Controller Update

--- src/main/java/kr/co/strato/mcmp/catalog/controller/CatalogController.java
+++ src/main/java/kr/co/strato/mcmp/catalog/controller/CatalogController.java
@@ -11,35 +11,35 @@ import kr.co.strato.mcmp.api.response.ResponseWrapper;
 import kr.co.strato.mcmp.catalog.model.Catalog;
 import kr.co.strato.mcmp.catalog.service.CatalogService;
 
-//@Tag(name = "Catalog", description = "카탈로그 관리")
+@Tag(name = "Catalog", description = "카탈로그 관리")
 @RequestMapping("/catalog")
 @RestController
 public class CatalogController {
 	
-//	@Autowired
-//	private CatalogService calgSvc;
-//
-//	@Operation(summary = "카탈로그 목록 조회")
-//	@GetMapping("/list")
-//	public ResponseWrapper<List<Catalog>> getCatalogList(@RequestParam int nexusId) {
-//		return null;
-//	}
-//
-//	@Operation(summary = "nexus repository 목폭조회")
-//	@GetMapping("/nexus/{nexusId}")
-//	public ResponseWrapper<List<Catalog>> getNexusRepository(@PathVariable int nexusId) {
-//		return null;
-//	}
-//
-//	@Operation(summary = "nexus repository 구성 목록조회")
-//	@GetMapping("/nexus/{nexusId}/{repository}")
-//	public ResponseWrapper<List<Catalog>> getNexusRepositoryIn(@PathVariable int nexusId, @PathVariable String repository) {
-//		return null;
-//	}
-//
-//	@Operation(summary = "nexus file 등록(lib/jar/tar/etc...)")
-//	@PostMapping("/nexus/{nexusId}/{repository}")
-//	public ResponseWrapper<List<Catalog>> setLibNexus() {
-//		return null;
-//	}
+	@Autowired
+	private CatalogService calgSvc;
+
+	@Operation(summary = "카탈로그 목록 조회")
+	@GetMapping("/list")
+	public ResponseWrapper<List<Catalog>> getCatalogList(@RequestParam int nexusId) {
+		return null;
+	}
+
+	@Operation(summary = "nexus repository 목폭조회")
+	@GetMapping("/nexus/{nexusId}")
+	public ResponseWrapper<List<Catalog>> getNexusRepository(@PathVariable int nexusId) {
+		return null;
+	}
+
+	@Operation(summary = "nexus repository 구성 목록조회")
+	@GetMapping("/nexus/{nexusId}/{repository}")
+	public ResponseWrapper<List<Catalog>> getNexusRepositoryIn(@PathVariable int nexusId, @PathVariable String repository) {
+		return null;
+	}
+
+	@Operation(summary = "nexus file 등록(lib/jar/tar/etc...)")
+	@PostMapping("/nexus/{nexusId}/{repository}")
+	public ResponseWrapper<List<Catalog>> setLibNexus() {
+		return null;
+	}
 }
