9538161ec3d2f2bf9e5e7ddba76afb6f951ee9d6
ysh <ysh0208@strato.co.kr>
2024-08-20 10:49:00 +0900

    Document 최신화

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@
 INSERT INTO oss_type (oss_type_idx, oss_type_name, oss_type_desc) VALUES (1, 'JENKINS', 'init');
 
 -- Step 2: Insert into oss
-INSERT INTO oss (oss_idx, oss_type_idx, oss_name, oss_desc, oss_url, oss_username, oss_password) VALUES (1, 1, 'SampleOss', 'Sample Description', 'http://sample.com', 'root', '1234');
+INSERT INTO oss (oss_idx, oss_type_idx, oss_name, oss_desc, oss_url, oss_username, oss_password) VALUES (1, 1, 'SampleOss', 'Sample Description', 'http://sample.com', 'root', null);
 
 -- Step 3: Insert into workflow_stage_type (assuming this table exists and 1 is valid)
 -- Note: No specific values provided for WorkflowStageType. Adjust as necessary.
@@ -16,7 +16,7 @@ INSERT INTO workflow (workflow_idx, workflow_name, workflow_purpose, oss_idx, sc
 -- INSERT INTO workflow (workflow_idx, workflow_name, workflow_purpose, oss_idx, script) VALUES (1, 'Sample Workflow', 'Sample Purpose', 1, 'Sample Script');
 
 -- Step 6: Insert into workflow_param
-INSERT INTO workflow_param (param_idx, workflow_idx, param_key, param_value) VALUES (1, 1, 'Sample Key', 'Sample Value');
+INSERT INTO workflow_param (param_idx, workflow_idx, param_key, param_value, event_listener_yn) VALUES (1, 1, 'Sample Key', 'Sample Value', 'N');
 
 -- Step 7: Insert into workflow_stage_mapping
-INSERT INTO workflow_stage_mapping (mapping_idx, workflow_idx, stage_order, workflow_stage_type_idx, stage) VALUES (1, 1, 1, 1, 'Sample Stage Content');
+INSERT INTO workflow_stage_mapping (mapping_idx, workflow_idx, stage_order, workflow_stage_idx, stage) VALUES (1, 1, 1, 1, 'Sample Stage Content');
