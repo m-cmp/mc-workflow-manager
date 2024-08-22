2970a9dc4c2125b54442a4741baaeebce15fe621
ysh <ysh0208@strato.co.kr>
2024-08-22 14:53:13 +0900

    Default Script 수정

--- src/main/java/kr/co/mcmp/workflow/service/jenkins/JenkinsPipelineGeneratorService.java
+++ src/main/java/kr/co/mcmp/workflow/service/jenkins/JenkinsPipelineGeneratorService.java
@@ -42,58 +42,21 @@ public class JenkinsPipelineGeneratorService {
 
 		JenkinsPipelineUtil.appendLine(sb, "\n");
 
-		JenkinsPipelineUtil.appendLine(sb, "def getSSHKey(jsonInput){\n" +
+		JenkinsPipelineUtil.appendLine(sb, "" +
+				"import groovy.json.JsonSlurper\n" +
+				"\n" +
+				"def getSSHKey(jsonInput) {\n" +
 				"    def json = new JsonSlurper().parseText(jsonInput)\n" +
-				"    def eachPemVal = '';\n" +
-				"    json.each { myData ->\n" +
-				"        if(eachPemVal != ''){return true}\n" +
-				"        if(myData.key == 'McisSubGroupAccessInfo'){\n" +
-				"            def v1 = myData.value\n" +
-				"            v1.each{vv ->\n" +
-				"                if(eachPemVal != ''){return true}\n" +
-				"                vv.each{\n" +
-				"                    vv2 ->\n" +
-				"                    if(eachPemVal != ''){return true}\n" +
-				"                    if(vv2.key == \"McisVmAccessInfo\"){\n" +
-				"                        def v2 = vv2.value\n" +
-				"                        v2.each{\n" +
-				"                            vv3 ->\n" +
-				"                            if(eachPemVal != ''){return true}\n" +
-				"                            vv3.each{\n" +
-				"                                vvv3 ->\n" +
-				"                                if(eachPemVal != ''){return true}\n" +
-				"                                 if(vvv3.key == \"privateKey\"){\n" +
-				"                                    eachPemVal = vvv3.value;\n" +
-				"                                    return true;\n" +
-				"                                 }\n" +
-				"                            }\n" +
-				"                        } \n" +
-				"                    } \n" +
-				"                }\n" +
-				"            }\n" +
-				"        }\n" +
-				"    }\n" +
-				"    return eachPemVal\n" +
-				"}");
-
-		JenkinsPipelineUtil.appendLine(sb, "\n");
-
-		JenkinsPipelineUtil.appendLine(sb, "def getPublicInfoList(jsonInput){\n" +
+				"    return json.findResult { it.key == 'McisSubGroupAccessInfo' ? \n" +
+				"        it.value.findResult { it.McisVmAccessInfo?.findResult { it.privateKey } } : null \n" +
+				"    } ?: ''\n" +
+				"}\n" +
+				"\n" +
+				"def getPublicInfoList(jsonInput) {\n" +
 				"    def json = new JsonSlurper().parseText(jsonInput)\n" +
-				"    def eachPemVal = [];\n" +
-				"    json.each { myData ->\n" +
-				"        if(myData.key == 'McisSubGroupAccessInfo'){\n" +
-				"           def v1 = myData.value\n" +
-				"           def v1ary = v1.McisVmAccessInfo\n" +
-				"           v1ary.each{ v2 -> \n" +
-				"                v2.each{ v3 ->\n" +
-				"                   eachPemVal.add(v3.publicIP)\n" +
-				"                }\n" +
-				"           }\n" +
-				"        }\n" +
-				"    }\n" +
-				"    return eachPemVal\n" +
-				"}");
+				"    return json.findAll { it.key == 'McisSubGroupAccessInfo' }\n" +
+				"        .collectMany { it.value.McisVmAccessInfo*.publicIP }\n" +
+				"}\n");
 
 		JenkinsPipelineUtil.appendLine(sb, "\n");
 
