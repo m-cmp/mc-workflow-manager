0567a514fdca68a257de119d298f7b3d147df3ac
ysh <ysh0208@strato.co.kr>
2024-08-26 11:10:07 +0900

    shell update

--- script/build-mc-workflow.sh
+++ script/build-mc-workflow.sh
@@ -13,10 +13,10 @@ cd ${PROJECT_PATH}
 sudo ./gradlew clean build -x test
 echo "build mc-workflow-manager"
 
-sudo chmod +x ${PROJECT_PATH}/build/libs/workflow_manager-0.2.1.jar
+sudo chmod +x ${PROJECT_PATH}/build/libs/mc-workflow-manager-0.2.1.jar
 
 echo "docker build"
-sudo docker build -f "${PROJECT_PATH}/script/Dockerfile" -t mc-workflow-manager:v0.0.1 .
+sudo docker build -f "${PROJECT_PATH}/script/Dockerfile" -t mc-workflow-manager:v0.2.1 .
 echo "======================================================================================"
 echo "WORKFLOW MANAGER BUILE FINISH"
 echo "======================================================================================"
